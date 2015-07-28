b7c3479447896dde3e042c51f1b05f0a1d7732ba
David Bindel <bindel@cs.cornell.edu>
2015-07-28 17:45:47 -0400

    Update to quiz demo.

--- js/quiz.js
+++ js/quiz.js
@@ -3,33 +3,37 @@
 
 $(document).ready(function() {
 
-    $(".quiz").each(function(i,obj) {
-        var data = $.parseJSON($(this).html());
-        var html = "<div style=\"text-align: left; display: inline-block;\">";
-        html += "<p>" + data.question + "</p>";
-        html += "<ul style=\"list-style: none;\">";
+    var re_A = /A:/;
+    var re_eq = /[=]:/;
 
-        nchoices = data.choices.length;
-        console.log("Walking through " + nchoices + " choices");
-        for (var i = 0; i < nchoices; ++i) {
-            var item = data.choices[i];
-            console.log("Item " + i + ": " + item);
+    $(".quizzer").each(function(i,obj) {
+        var qspec = $(this).html();
+        var qspec_lines = qspec.split(/\s+[A=]:/);
+        qspec_lines = qspec_lines.map(function(s) { return s.trim(); });
+        console.log(qspec_lines);
+        var tag = "_q_" + i;
+        var html = "<div style=\"text-align: left; display: inline-block;\">";
+        html += "<p>" + qspec_lines[0] + "</p>";
+        html += "<ul style=\"list-style: none;\">";
+        for (var i = 1; i < qspec_lines.length; i += 2) {
+            var answer = qspec_lines[i];
+            var reply = qspec_lines[i+1];
             html += "<li>";
             html += "<input type=\"radio\"";
-            html += " name=\"" + data.tag + "\"";
-            html += " id=\"" + data.tag + "_" + i + "\"";
-            html += " value=\"" + item.response + "\">";
-            html += "<label for=\"" + data.tag + "_" + i + "\">";
-            html += item.answer;
+            html += " name=\"" + tag + "\"";
+            html += " id=\"" + tag + "_" + i + "\"";
+            html += " value=\"" + reply + "\">";
+            html += "<label for=\"" + tag + "_" + i + "\">";
+            html += answer;
             html += "</label>";
             html += "</li>";
         }
         html += "</ul></div><br/>";
-        html += "<p style=\"text-align: left; display: inline-block;\" id=\"" + data.tag + "Result\"></p>";
-        console.log(html)
+        html += "<p style=\"text-align: left; display: inline-block;\" id=\"" + tag + "Result\"></p>";
+        console.log(html);
         $(this).replaceWith(html);
     });
-    
+
     $("input:radio").click(function() {
         $('#' + this.name + 'Result').text(this.value);
     });
